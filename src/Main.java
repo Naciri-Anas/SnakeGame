ea18a7e91a43a9b0dc4357dcc7784bf7b2c8b4b7
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2023-11-19 01:32:04 +0100

    Showing Jframe
    First try

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        int b
+        int boarHeight = 600;
+        int boardWidth = boarHeight;
 
+JFrame frame = new JFrame("Snake");
+frame.setVisible(true);
+frame.setSize(boardWidth,boarHeight);
+frame.setLocationRelativeTo(null);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
